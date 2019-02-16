e88f285198a605ea936f460d070f4e3476c923b2
Jesus Santa Olalla <jesus.santaolalla.picazo@gmail.com>
2019-02-16 12:58:30 -0600

    * Project structure

---
+++ Liverpool/Repository/BaseRepository.swift
@@ -0,0 +1,9 @@
+//
+//  BaseRepository.swift
+//  Liverpool
+//
+//  Created by Jesus Santa Olalla on 2/16/19.
+//  Copyright © 2019 jsolalla. All rights reserved.
+//
+
+import Foundation
---
+++ Liverpool/Repository/HomeRepository.swift
@@ -0,0 +1,9 @@
+//
+//  HomeRepository.swift
+//  Liverpool
+//
+//  Created by Jesus Santa Olalla on 2/16/19.
+//  Copyright © 2019 jsolalla. All rights reserved.
+//
+
+import Foundation
